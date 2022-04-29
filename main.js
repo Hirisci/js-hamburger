7f27c8fd8c9c15dc61f80730658e3e2327389144
Hirisci <www.alan92@icloud.com>
2022-04-29 14:41:07 +0200

    finito

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
+const hamburgerMenu= document.getElementsByClassName("hamburger-menu")[0];
+
 const btnOpen= document.getElementById("open-menu");
-const hamburgerMenu= document.getElementsByClassName("hamburger-menu");
-const btnClose= document.getElementById("close-menu");
-
-console.log(hamburgerMenu.Style());
-console.log(btnOpen);
-console.log(btnClose);
-btnOpen.addEventListener('click', 
-    function(){
-        
+btnOpen.addEventListener("click", 
+    function() {
+        hamburgerMenu.style = "display: block";
     }
-);
+); 
 
+const btnClose= document.getElementById("close-menu");
 btnClose.addEventListener('click', 
     function(){
-       
+        hamburgerMenu.style = "display: none";
     }
 );
 
