ffe7cd6f655f343f4c7c33418fb5e5ce0c38186e
Hirisci <www.alan92@icloud.com>
2022-04-29 14:16:34 +0200

    inserite variabili

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 const btnOpen= document.getElementById("open-menu");
-const hamburgerMenu= document.getElementsByClassName("hamburger-menu")
+const hamburgerMenu= document.getElementsByClassName("hamburger-menu");
 const btnClose= document.getElementById("close-menu");
 
-
+console.log(hamburgerMenu.Style());
+console.log(btnOpen);
+console.log(btnClose);
 btnOpen.addEventListener('click', 
     function(){
-
+        
     }
 );
 
 btnClose.addEventListener('click', 
     function(){
-
+       
     }
 );
+
