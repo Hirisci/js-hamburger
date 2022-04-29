33a8bdc2b32c689b092e6acfd308f65215b4a777
Hirisci <www.alan92@icloud.com>
2022-04-29 13:58:44 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const btnOpen= document.getElementById("open-menu");
+const hamburgerMenu= document.getElementsByClassName("hamburger-menu")
+const btnClose= document.getElementById("close-menu");
+
+
+btnOpen.addEventListener('click', 
+    function(){
+
+    }
+);
+
+btnClose.addEventListener('click', 
+    function(){
+
+    }
+);
